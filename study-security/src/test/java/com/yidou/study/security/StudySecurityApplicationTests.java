da4a1e6460cbb34bc3f4db8d363236014bb51d6a
pierai <it_quinna@163.com>
2023-07-13 20:08:53 +0800

    feat(学习security表单配置) 1. thymeleaf配置解释 2. 其他用于测试文件

---
+++ study-security/src/test/java/com/yidou/study/security/StudySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yidou.study.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
